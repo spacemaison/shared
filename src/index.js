8f2fa8704b06fd4421bd86fbf8dcb15d2aaf5dc4
Jesse Tuchsen <tuchsen@hush.ai>
2016-06-22 11:28:03 -0700

    Fix exporting bug
    The main index file was exporting the actions file as the application
    models.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import * as actions from './actions/index'
-import * as models from './actions/index'
+import * as models from './models/index'
 export * from './store'
 
 export { actions, models }
