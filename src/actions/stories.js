d37c57d1a15be1872fe6629cc75ba734ce6dadac
Jesse Tuchsen <tuchsen@hush.ai>
2016-08-19 15:45:54 -0700

    Import actions in stories directly rather than relying on the index

--- src/actions/stories.js
+++ src/actions/stories.js
@@ -1,9 +1,8 @@
-import { actions, fetch, timeouts } from './index'
+import * as fetch from './fetch'
+import { wrap, aggragate } from './action'
 import { STORIES_REFRESH, NEWS_REFRESH, LAUNCH_REFRESH } from './actions'
 import { NEWS_URL, LAUNCH_URL } from '../constants'
 import { StoryContainer } from '../models/index'
-
-const { wrap, aggragate } = actions
 
 export const launches = wrap(LAUNCH_REFRESH, function * (url = LAUNCH_URL) {
   return new StoryContainer(false, yield fetch.json(url))
