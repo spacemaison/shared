fb98c88f5b0194fb7550993f0987c21a84d934a1
Jesse Tuchsen <tuchsen@hush.ai>
2016-09-09 12:33:49 -0700

    Remove babel-polyfill import.
    This is causing issues in the frontend project because it's imported
    there, which throws an error for importing the project twice.

--- dist/src/index.js
+++ dist/src/index.js
@@ -1,7 +1,5 @@
 'use strict';Object.defineProperty(exports,"__esModule",{value:true});exports.models=exports.actions=undefined;var _store=require('./store');
 
-
-
-Object.keys(_store).forEach(function(key){if(key==="default")return;Object.defineProperty(exports,key,{enumerable:true,get:function get(){return _store[key];}});});require('babel-polyfill');var _index=require('./actions/index');var actions=_interopRequireWildcard(_index);var _index2=require('./models/index');var models=_interopRequireWildcard(_index2);function _interopRequireWildcard(obj){if(obj&&obj.__esModule){return obj;}else{var newObj={};if(obj!=null){for(var key in obj){if(Object.prototype.hasOwnProperty.call(obj,key))newObj[key]=obj[key];}}newObj.default=obj;return newObj;}}exports.
+Object.keys(_store).forEach(function(key){if(key==="default")return;Object.defineProperty(exports,key,{enumerable:true,get:function get(){return _store[key];}});});var _index=require('./actions/index');var actions=_interopRequireWildcard(_index);var _index2=require('./models/index');var models=_interopRequireWildcard(_index2);function _interopRequireWildcard(obj){if(obj&&obj.__esModule){return obj;}else{var newObj={};if(obj!=null){for(var key in obj){if(Object.prototype.hasOwnProperty.call(obj,key))newObj[key]=obj[key];}}newObj.default=obj;return newObj;}}exports.
 
 actions=actions;exports.models=models;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,3 @@
-import 'babel-polyfill'
-
 import * as actions from './actions/index'
 import * as models from './models/index'
 export * from './store'
