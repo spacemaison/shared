47c8a19c58b784151ba3325ff9ba1a1c26403203
Jesse Tuchsen <tuchsen@hush.ai>
2017-01-14 16:19:48 -0800

    Add export for action helpers to keep things dry

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,7 @@
+import * as action from './actions'
 import * as actions from './action'
 import * as fetch from './fetch'
 import * as stories from './stories'
 import * as timeouts from './timeout'
 
-export { actions, fetch, stories, timeouts }
+export { action, actions, fetch, stories, timeouts }
