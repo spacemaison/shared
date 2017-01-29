d42d07270cc43ff52be3d57f155bdc412c5d7bf1
Jesse Tuchsen <tuchsen@hush.ai>
2017-01-28 17:05:38 -0800

    Implement persistent storage

--- src/store.js
+++ src/store.js
@@ -1,27 +1,45 @@
-import { applyMiddleware, combineReducers } from 'redux'
+import {
+    applyMiddleware,
+    combineReducers,
+    createStore as _createStore } from 'redux'
 import * as storage from 'redux-storage'
-import { createStore as _createStore } from 'redux'
 import generatorMiddleware from 'redux-gen'
+import filterStorageEngine from 'redux-storage-decorator-filter'
 import interceptFetch from './middleware/fetch'
 import timeoutMiddleware from './middleware/timeout'
 import debugInterceptFetch from './middleware/fetch-debug'
 import * as sharedReducers from './reducers/index'
+import * as actions from './actions/actions'
 
 export function createStore (
       addedReducers = {},
       addedMiddleware = [],
       options = {}) {
   const { globalErrorHandler, globalSuccessHandler } = options
+  const storageEngine = filterStorageEngine(options.storageEngine, [
+    'reminders'
+  ])
+  const storageMiddleware = storage.createMiddleware(storageEngine, [], [
+    actions.REMINDER_ADD,
+    actions.REMINDER_REMOVE,
+    actions.REMINDER_TOGGLE
+  ])
+  const storageLoader = storage.createLoader(storageEngine)
   const fetchMiddleware = options.debug === true
       ? debugInterceptFetch
       : interceptFetch
 
   const reducers = Object.assign({}, addedReducers, sharedReducers)
-  return _createStore(
+  const store = _createStore(
       storage.reducer(combineReducers(reducers)),
       applyMiddleware(
           generatorMiddleware(globalErrorHandler, globalSuccessHandler),
           fetchMiddleware,
           timeoutMiddleware,
+          storageMiddleware,
           ...addedMiddleware))
+
+  storageLoader(store)
+
+  return store
 }
