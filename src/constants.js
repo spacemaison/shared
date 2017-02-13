3c0d797e837ccd8de4e37ae9ae3493be52af14ad
Jesse Tuchsen <tuchsen@hush.ai>
2017-02-12 16:17:16 -0800

    Change project name.
    This is just to ensure that the project gets a cheap URL. Maison, if you
    were wondering, is 'house' in French, which makes very little sense for
    a news aggregator about space, but the domain name is 7 bucks a year,
    and the American English pronunciation of space.maison sounds nice if
    you pronounce it like space.mason, which also wouldn't make any sense,
    but still sounds nice. This commit message reads like a bad parody of
    Nate DiMeo's Memory Palace.

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,11 @@
 import url from 'url'
 
-export const namespace = url.parse('http://localhost:3000')
+export const PROTOCOL = process.env.SPACE_MAISON_PROTOCOL || 'http:'
+export const HOST = process.env.SPACE_MAISON_HOSTNAME || 'localhost'
+export const PORT = process.env.SPACE_MAISON_PORT || '3000'
+export const URL = `${PROTOCOL}//${HOST}${PORT ? ':' + PORT : ''}`
+  
+export const namespace = url.parse(URL)
 export const NEWS_URL = namespace.resolve('stories/news')
 export const LAUNCH_URL = namespace.resolve('stories/launches')
 export const MEDIA_URL = namespace.resolve('stories/media')
