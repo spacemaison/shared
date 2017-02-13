8e213a3603e9c1a830bd49f18fd020db74fecd9b
Jesse Tuchsen <tuchsen@hush.ai>
2017-02-12 16:48:29 -0800

    Add 'date' and 'type' fields to MediaStory Model

--- src/models/MediaStory.js
+++ src/models/MediaStory.js
@@ -4,10 +4,12 @@ import { Image } from './Image'
 export class MediaStory extends Model {
   get [fields] () {
     return Object.assign(super[fields], {
+      date: Date,
       title: String,
       description: String,
       url: String,
-      image: Image
+      image: Image,
+      type: String
     })
   }
 }
