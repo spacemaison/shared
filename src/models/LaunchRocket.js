27c2311b421a1a793a7ef7fdd78a3fdecd46f336
Jesse Tuchsen <tuchsen@hush.ai>
2017-02-26 18:42:41 -0800

    Refactor LaunchRocket to use Image type + add a default Image

--- src/models/LaunchRocket.js
+++ src/models/LaunchRocket.js
@@ -1,6 +1,24 @@
 import { Model, fields } from './Model'
+import { Image } from './Image'
+
+const STOCK_PHOTO_SIZES = [
+  320, 480, 640, 720, 768, 800, 960, 1024, 1080, 1280, 1440, 1920
+]
+const getStockPhotoURLWithWidth = size => (
+  `https://s3.amazonaws.com/launchlibrary/RocketImages/placeholder_${size}.png`
+)
 
 export class LaunchRocket extends Model {
+  constructor ({ image } = {}) {
+    super(...arguments)
+
+    this.image = new Image(image || {
+      urls: STOCK_PHOTO_SIZES.map(getStockPhotoURLWithWidth),
+      sizes: STOCK_PHOTO_SIZES.map(width => [ width, width / (16 / 10) ]),
+      descriptions: [ 'Stock rocket photo' ]
+    })
+  }
+
   get [fields] () {
     return Object.assign(super[fields], {
       configuration: String,
@@ -10,8 +28,7 @@ export class LaunchRocket extends Model {
       family: Object,
       wikiURL: String,
       infoURLs: Array,
-      imageURL: String,
-      imageSizes: Array
+      image: Image
     })
   }
 }
